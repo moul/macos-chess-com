c4465c620fb20b1e1aa7a1e316bdcc186ea1ad00
Manfred Touron <94029+moul@users.noreply.github.com>
2021-04-14 09:50:40 +0200

    Initial Commit

---
+++ chess-com/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  chess-com
+//
+//  Created by Manfred Touron on 14/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
